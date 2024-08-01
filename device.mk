4192ca750f0aa4ca69eb93a0a2a71d1f936dcf9e
chiteroman <98092901+chiteroman@users.noreply.github.com>
2024-08-01 23:44:04 +0200

    pipa: Drop device Id attestation
    * SM8250's TEE does not support device id attestation
    
    Change-Id: I25dd356311ea17cf741df54f2de0a0152426d847

--- device.mk
+++ device.mk
@@ -38,10 +38,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 
-# Attestation
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
-
 # Audio
 PRODUCT_PACKAGES += \
     libqcompostprocbundle \
