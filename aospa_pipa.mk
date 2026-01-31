55b8a4c49c32003a484d65343da2867a45be71a1
CuriousNom <aryanbiswas7585@gmail.com>
2026-01-31 12:40:41 +0530

    pipa: Set device build fingerprint
    Change-Id: I8b982ba2553006ddc60f61623e5fe027dfa9af63

--- aospa_pipa.mk
+++ aospa_pipa.mk
@@ -32,3 +32,11 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := 23043RP34G
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_SYSTEM_NAME := pipa_cn
+PRODUCT_SYSTEM_DEVICE := pipa
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildFingerprint=Xiaomi/pipa_pad_cn/pipa:14/UKQ1.240624.001/OS2.0.16.0.UMZCNXM:user/release-keys \
+    DeviceName=$(PRODUCT_SYSTEM_DEVICE) \
+    DeviceProduct=$(PRODUCT_SYSTEM_NAME)
