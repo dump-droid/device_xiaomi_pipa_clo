4cfb634990f5fc8f7fd67296b659f589be611274
Bin <9633157+web1n@users.noreply.github.com>
2026-01-31 13:17:28 +0530

    pipa: Patch audio library to load vndk34
    Change-Id: I0518778af917e72b50be5995783a20149eb951e8

--- extract-files.sh
+++ extract-files.sh
@@ -78,6 +78,9 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             grep -q "libcrypto_shim.so" "${2}" || "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
             ;;
+        vendor/lib/libaudioroute_ext.so|vendor/lib/hw/audio.primary.kona.so)
+            "${PATCHELF}" --replace-needed "libaudioroute.so" "libaudioroute-v34.so" "${2}"
+            ;;
         *)
             return 1
             ;;
