be96d562c49a9da74ffd9997f93895f0aa451b80
CuriousNom <aryanbiswas7585@gmail.com>
2026-02-03 17:38:48 +0100

    pipa: Reduce SF timer to 2s
    * 4s is overkill in my opinion.

--- device.mk
+++ device.mk
@@ -214,8 +214,8 @@ PRODUCT_ODM_PROPERTIES += \
     vendor.display.use_smooth_motion=0
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.set_idle_timer_ms=4000 \
-    ro.surface_flinger.set_touch_timer_ms=4000 \
+    ro.surface_flinger.set_idle_timer_ms=2000 \
+    ro.surface_flinger.set_touch_timer_ms=2000 \
     ro.surface_flinger.set_display_power_timer_ms=1000 \
     ro.surface_flinger.use_content_detection_for_refresh_rate=true
 
